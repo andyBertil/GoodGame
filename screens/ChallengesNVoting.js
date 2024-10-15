6b00a9e6edfd21dbacf4308379eede1b560da213
Andy <151898439+andyBertil@users.noreply.github.com>
2024-10-15 01:12:27 -0400

    Add files via upload

---
+++ screens/ChallengesNVoting.js
@@ -0,0 +1,9 @@
+import { SafeAreaView, Text, View } from "react-native";
+
+export default function ChallengesNVoting() {
+  return (
+    <SafeAreaView>
+        <Text> </Text>
+    </SafeAreaView>
+  );  
+}
